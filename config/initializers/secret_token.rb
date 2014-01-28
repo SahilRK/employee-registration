871dc60f3b325aae85a76a98c54b540b30a51919
SahilRK <sahilkulkarni.bca@gmail.com>
2014-01-28 17:28:00 +0530

    Initial Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+EmployeeRegistration::Application.config.secret_key_base = 'b0e4a4705e29cf04d458aad99342f0f2a2bc88e9a1347e9afaca643540043c70d615e35952a9df4bbbd1b3a8eb0bc80cad5368158b9fecc92d2dbf7121e45d7c'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+EmployeeRegistration::Application.config.session_store :cookie_store, key: '_employee-registration_session'
